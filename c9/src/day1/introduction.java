952e8a7d255159dde22e5532b6edcbb380aa0696
rajsa <rajsa@LAPTOP-AG3RTCVE>
2023-03-30 12:22:46 -0700

    FIRST COMMIT

---
+++ c9/src/day1/introduction.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class introduction {
+   public static void main(String[] args) {
+	  System.out.println("welcome");
+	  System.out.println("nice to meet you");
+	  System.out.println("intro");
+   }
+}
