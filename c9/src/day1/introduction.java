6bf30a4b5e93d952989c848f5e103107055776fa
HemasowndariKarthikeyan <129287608+HemasowndariKarthikeyan@users.noreply.github.com>
2023-04-25 08:23:15 -0700

    Delete c9 directory

--- c9/src/day1/introduction.java
+++
@@ -1,9 +0,0 @@
-package day1;
-
-public class introduction {
-   public static void main(String[] args) {
-	  System.out.println("welcome");
-	  System.out.println("nice to meet you");
-	  System.out.println("intro");
-   }
-}
